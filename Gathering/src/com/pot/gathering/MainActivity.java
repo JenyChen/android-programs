63e9573776fb9dc3bac274fb4ef45cdbbc159978
JenyChen <1031359915@qq.com>
2015-03-31 17:55:05 +0800

    test

--- Gathering/src/com/pot/gathering/MainActivity.java
+++ Gathering/src/com/pot/gathering/MainActivity.java
@@ -1,6 +1,15 @@
 package com.pot.gathering;
 
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.pot.gathering.http.AsynHttpManager;
+
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends BaseActivity {
 
@@ -8,5 +17,30 @@ public class MainActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		test();
+	}
+	
+	private void test(){
+		String url = "http://10.24.50.14:8550/user/GetUsers";
+		RequestParams params = new RequestParams();
+		params.put("name", "admin");
+		params.setContentEncoding("utf-8");
+		AsynHttpManager.getInstance().get(this, url, params, new AsyncHttpResponseHandler(){
+
+			@Override
+			public void onFailure(int arg0, Header[] arg1, byte[] arg2,
+					Throwable arg3) {
+				// TODO Auto-generated method stub
+				String s = new String(arg2);
+			}
+
+			@Override
+			public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+				// TODO Auto-generated method stub
+				String s = new String(arg2);
+				Log.v("Jeny", "s=="+s);
+			}
+		});
 	}
 }
--- Gathering/src/com/pot/gathering/http/AsynHttpManager.java
+++ Gathering/src/com/pot/gathering/http/AsynHttpManager.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpEntity;
 import android.content.Context;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.ResponseHandlerInterface;
 
@@ -28,5 +29,9 @@ public class AsynHttpManager {
 	
 	public void post(Context context, String url, HttpEntity entity, ResponseHandlerInterface responseHandler){
 		httpClient.post(context, url, entity, "application/json", responseHandler);
+	}
+	
+	public void get(Context context, String url, RequestParams params, AsyncHttpResponseHandler responseHandler){
+		httpClient.get(context, url, params, responseHandler);
 	}
 }
