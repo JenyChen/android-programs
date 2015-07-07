d0b2ef201712ed1fe1aba7145218935f59f070e2
JenyChen <1031359915@qq.com>
2015-07-07 11:19:57 +0800

    添加错误反馈

--- Gathering/src/com/pot/gathering/BaseActivity.java
+++ Gathering/src/com/pot/gathering/BaseActivity.java
@@ -1,5 +1,8 @@
 package com.pot.gathering;
 
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.FeedbackManager;
+import net.hockeyapp.android.UpdateManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,52 +12,61 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class BaseActivity extends Activity implements OnClickListener{
+import com.pot.gathering.tool.DeviceUuidFactory;
+
+public class BaseActivity extends Activity implements OnClickListener {
 
 	private TextView textTitle;
 	private LinearLayout layoutBack;
 	private ImageView imgBack;
 	private TextView textTitleLeft;
+	private DeviceUuidFactory mDeviceUuidFactory;
+	private String APP_ID;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
+		mDeviceUuidFactory = new DeviceUuidFactory(this);
+		APP_ID = mDeviceUuidFactory.getDeviceUuid().toString();
+		checkForUpdates();
 	}
-	
+
 	/**
 	 * 设置标题样式
-	* @Title: setTitleStyle
-	* @Description: TODO
-	* @param @param includBack 如果需要返回键，则为true；不然就显示为中间标题
-	* @return void
-	* @throws
+	 * 
+	 * @Title: setTitleStyle
+	 * @Description: TODO
+	 * @param @param includBack 如果需要返回键，则为true；不然就显示为中间标题
+	 * @return void
+	 * @throws
 	 */
-	protected void setTitleStyle(boolean includBack){
+	protected void setTitleStyle(boolean includBack) {
 		textTitle = (TextView) findViewById(R.id.text_title);
 		layoutBack = (LinearLayout) findViewById(R.id.layout_back);
 		imgBack = (ImageView) findViewById(R.id.img_back);
 		textTitleLeft = (TextView) findViewById(R.id.text_title_left);
-		
-		if(includBack){
+
+		if (includBack) {
 			textTitle.setVisibility(View.GONE);
 			layoutBack.setVisibility(View.VISIBLE);
-		}else{
+		} else {
 			textTitle.setVisibility(View.VISIBLE);
 			layoutBack.setVisibility(View.GONE);
 		}
-		
+
 		layoutBack.setOnClickListener(this);
 	}
-	
-	protected void setTitle(boolean includBack, String title){
-		if(includBack){
+
+	protected void setTitle(boolean includBack, String title) {
+		if (includBack) {
 			textTitleLeft.setText(title);
-		}else{
+		} else {
 			textTitle.setText(title);
 		}
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -66,4 +78,51 @@ public class BaseActivity extends Activity implements OnClickListener{
 			break;
 		}
 	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		checkForCrashes();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		unregisterManagers();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		unregisterManagers();
+	}
+
+	private void checkForCrashes() {
+		CrashManager.register(this, APP_ID);
+	}
+
+	private void checkForUpdates() {
+		// Remove this for store builds!
+		UpdateManager.register(this, APP_ID);
+	}
+
+	private void unregisterManagers() {
+		UpdateManager.unregister();
+		// unregister other managers if necessary...
+	}
+
+	/**
+	 * Call the method showFeedbackActivity in an onClick,
+	 * onMenuItemSelected, or onOptionsItemSelected listener method.
+	* @Title: showFeedbackActivity
+	* @Description: TODO
+	* @param 
+	* @return void
+	* @throws
+	 */
+	public void showFeedbackActivity() {
+		FeedbackManager.register(this, APP_ID, null);
+		FeedbackManager.showFeedbackActivity(this);
+	}
+
 }
